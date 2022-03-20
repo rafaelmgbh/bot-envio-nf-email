7364d2fa753e371039adce7da395fa6173d78ca0
Rafael Santos de Araujo <rafael.santos@dev.simbioseventures.com>
2022-03-20 19:52:59 -0300

    Ajuste do input para password , Formatação do texto do Email

--- EnvioEmailNfs.py
+++ EnvioEmailNfs.py
@@ -5,11 +5,24 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import getpass
+
+
+
 host = "smtp.gmail.com"
 port = "587"
-login = ""
-senha = ""
-destinatario = ""
+
+destinatario = "rafael.santos@dev.simbioseventures.com"
+
+print("**********************************************")
+print(" Automação Envio NFs e Boleto Simbiose ")
+print("**********************************************")
+
+login = input("Qual seu E-mail: ")
+senha = getpass.getpass('Senha:')
+salario = int(input("Salário Base : R$ "))
+desc_terapia = int(input("Informe o Valor da Terapia : R$  "))
+desc_ingles = int(input("Informe o Valor do Ingles : R$  "))
 
 server = smtplib.SMTP(host,port)
 
@@ -17,10 +30,14 @@ server.ehlo()
 server.starttls()
 server.login(login,senha)
 
-corpo = """ Ola Tudo bom ?
-
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. </> 
-
+corpo = f""" Ola Tudo bom ?<br>
+Segue Boleto , NFs e XML <br>
+O Salário Base e de <b>R$ {salario}</b> <br>
+Desconto da Terapia  e de R$ {desc_terapia}<br>
+Desconto do Ingles e de R$ {desc_ingles}<br>
+<br>
+<b>O Salário Liquido e de R$ {salario-(desc_terapia+desc_ingles)}</b>
+<br><br>
 <p>Abs</p>
 <p> Rafael Santos </>
 
@@ -30,20 +47,42 @@ corpo = """ Ola Tudo bom ?
 email_msg = MIMEMultipart()
 email_msg['From'] = login
 email_msg['To'] = destinatario
-email_msg['Subject'] = "Meu e-mail enviado por Rafael"
+email_msg['Subject'] = "Rafael Santos NFs e BOLETO"
 email_msg.attach(MIMEText(corpo,'html'))
 
 #Lendo Binario do Arquivo a ser Anexado
 cam_arquivo_1 = "/home/rafael/Documentos/nfs_bot/Rafael_Santos_De_Araujo_Boleto.pdf"
-attchment = open(cam_arquivo_1,'rb')
+cam_arquivo_2 = "/home/rafael/Documentos/nfs_bot/Rafael_Santos_De_Araujo_NFS.pdf"
+cam_arquivo_3 = "/home/rafael/Documentos/nfs_bot/nfse_202200000000002.xml"
 
+attchment = open(cam_arquivo_1,'rb')
 att = MIMEBase('application', 'octet-stream')
 att.set_payload(attchment.read())
 encoders.encode_base64(att)
-
 att.add_header('Content-Disposition', f'attachment; filename = Rafael_Santos_De_Araujo_Boleto.pdf')
 attchment.close()
 email_msg.attach(att)
 
+attchment = open(cam_arquivo_2,'rb')
+att = MIMEBase('application', 'octet-stream')
+att.set_payload(attchment.read())
+encoders.encode_base64(att)
+att.add_header('Content-Disposition', f'attachment; filename = Rafael_Santos_De_Araujo_NFS.pdf')
+attchment.close()
+email_msg.attach(att)
+
+attchment = open(cam_arquivo_3,'rb')
+att = MIMEBase('application', 'octet-stream')
+att.set_payload(attchment.read())
+encoders.encode_base64(att)
+att.add_header('Content-Disposition', f'attachment; filename = nfse_202200000000002.xml')
+attchment.close()
+email_msg.attach(att)
+
+
 server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
 server.quit()
+
+print("**********************************************")
+print(" NFs e Boleto Enviado com Suceso ! ")
+print("**********************************************")
